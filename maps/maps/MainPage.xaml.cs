195de4b81bf2830e03135dac0b61f48109eed7c5
Kevin-Galarza77 <jimenezkev1040@gmail.com~>
2023-07-14 16:35:38 -0500

    Finish

---
+++ maps/maps/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
+using Xamarin.Essentials;
+
+
+namespace maps
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetLocation();
+        }
+
+        private async void GetLocation()
+        {
+            var request = new GeolocationRequest(GeolocationAccuracy.Default);
+            var location = await Geolocation.GetLocationAsync(request);
+
+            if (location != null)
+            {
+                var defaultPin = new Pin
+                {
+                    Type = PinType.Place,
+                    Label = "My Location",
+                    Position = new Position(location.Latitude, location.Longitude)
+                };
+
+                map.Pins.Add(defaultPin);
+                map.MoveToRegion(MapSpan.FromCenterAndRadius(defaultPin.Position, Distance.FromKilometers(1)));
+            }
+        }
+
+    }
+}
